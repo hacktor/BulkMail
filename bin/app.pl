0957ee8b91a8e1e32e40f050a2db1c78c93b422a
Ruben de Groot <ruben@hacktor.com>
2020-07-02 14:26:49 +0200

    Working reply mail, link with key

--- bin/app.pl
+++ bin/app.pl
@@ -1,4 +1,62 @@
 #!/usr/bin/env perl
 use Dancer;
 use BulkMail;
+use threads;
+use Net::POP3;
+use Email::Simple;
+use Email::Sender::Simple qw(sendmail);
+use Email::Simple::Creator;
+
+# create a thread polling for new mail
+threads->create(sub { 
+
+    my $db = BulkMail::init_db();
+    my $st = $db->prepare( config->{sqlite}{insert} );
+
+    for (;;) {
+        my $pop = Net::POP3->new(config->{email}{host}, SSL => 1, Timeout => 60, SSL_ca_file => config->{email}{ca});
+        if ($pop->login(config->{email}{user}, config->{email}{pass}) > 0) {
+    
+            my $msgnums = $pop->list; # hashref of msgnum => size
+            foreach my $msgnum (keys %$msgnums) {
+    
+                my $email = Email::Simple->new(join '', @{ $pop->get($msgnum) });
+                my $key = makeKey();
+                $st->execute($key,$email->header("From"),$email->header("Subject"),$email->header("Date"),$email->body);
+                sendReceipt($email,$key);
+    
+                $pop->delete($msgnum);
+            }
+        }
+        $pop->quit;
+        sleep 30;
+    }
+})->detach();
+
 dance;
+
+### helper subs ###
+
+sub makeKey {
+    my @chars = ('0'..'9', 'A'..'Z');
+    my $len = 64;
+    my $string;
+    while($len--){ $string .= $chars[rand @chars] };
+    $string;
+}
+
+sub sendReceipt {
+
+    my ($email,$key) = @_;
+    my $reply = Email::Simple->create(
+        header => [
+            To      => $email->header("From"),
+            From    => $email->header("To"),
+            Subject => "Received: " . $email->header("Subject"),
+        ],
+        body => template 'body' => { bulkurl => config->{bulkurl}, key => $key }, { layout => undef },
+    );
+
+    sendmail($reply);
+    print "Reply to ". $reply->header("To") ." send\n";
+}
--- lib/BulkMail.pm
+++ lib/BulkMail.pm
@@ -1,9 +1,38 @@
 package BulkMail;
 use Dancer ':syntax';
+use DBI;
 
 our $VERSION = '0.1';
 
-get '/' => sub {
+sub connect_db {
+  my $db = DBI->connect("dbi:SQLite:dbname=".config->{sqlite}{db}) or
+     die $DBI::errstr;
+
+  return $db;
+}
+
+sub init_db {
+    my $db = connect_db();
+
+    # slurp sql schema
+    my $schema = do { local(@ARGV,$/) = config->{sqlite}{schema}; <>};
+    $db->do($schema) or die $db->errstr;
+    return $db;
+}
+
+get '/mailing/:key' => sub {
+
+    my $db = connect_db();
+    my $stm = $db->prepare("select * from mbox where key = ?");
+    $stm->execute(param('key'));
+    if (my $row = $stm->fetchrow_hashref()) {
+        template 'mailing', { subject => $row->{subject}, from => $row->{from_address}, date => $row->{date} };
+    } else {
+        template 'index';
+    }
+};
+
+any ['get', 'post'] => '/' => sub {
     template 'index';
 };
 
