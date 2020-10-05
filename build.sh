c36f39f4c7ef3d47470d7a183e951cc854784c9b
Ruben de Groot <r.degroot@tudelft.nl>
2020-10-05 22:26:55 +0200

    build

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ GATEWAY=10.88.0.1
 newcontainer=$(buildah from ubuntu:focal)
 
 buildah config --created-by "TU Delft ICT-SYS-LIN"  $newcontainer
-buildah config --author "Ruben de Groot" --label name=nagiospod $newcontainer
+buildah config --author "Ruben de Groot" --label name=bulkmail $newcontainer
 buildah config --env CNI_NET=${CNI_NET} --env GATEWAY=${GATEWAY} $newcontainer
 buildah run $newcontainer apt update
-buildah run $newcontainer apt -y install libdancer-perl libhtml-template-perl libtemplate-perl libemail-simple-perl libemail-sender-perl libemail-address-xs-perl libnet-imap-client-perl libdbd-sqlite3-perl
+buildah run $newcontainer apt -y install libdancer-perl libhtml-template-perl libtemplate-perl libemail-simple-perl libemail-sender-perl libemail-address-xs-perl libmail-imapclient-perl libdbd-sqlite3-perl
 buildah copy $newcontainer bin /root/bin
 buildah copy $newcontainer config.yml /root/
 buildah copy $newcontainer environments /root/environments
@@ -18,6 +18,6 @@ buildah copy $newcontainer lib /root/lib
 buildah copy $newcontainer public /root/public
 buildah copy $newcontainer views /root/views
 buildah config --port 3000 $newcontainer
-buildah config --cmd "cd /root/; ./bin/app.pl" $newcontainer
+buildah config --cmd 'bash -c "cd /root/; ./bin/app.pl"' $newcontainer
 buildah commit $newcontainer bulkmail:latest
 
